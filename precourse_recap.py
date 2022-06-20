9a2e5f4ccb9d70d1d98ad1651d66d6d2fad1e948
Stuart-McCafferty <Stuartj678@gmail.com>
2022-06-20 23:49:09 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,13 @@
+firstName = "stuart"
+lastName = "mcCafferty"
+fullName = firstName.capitalize()+ lastName.capitalize()
+
+print(firstName)
+print(fullName)
+
+
+number1 = 10
+number2 = "20"
+sum = number1 + int(number2)
+print(sum)
+print(sum / 10)
